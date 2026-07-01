1bc45c411c5df8b01f46b311fe986e60f67b369f
Blandine-Lab <blandinelamadekota@gmail.com>
2026-07-01 22:27:28 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// ✅ Import d'EmailJS
+import emailjs from '@emailjs/browser';
+
+// ✅ Initialisation globale (une seule fois au démarrage de l'app)
+emailjs.init(import.meta.env.VITE_EMAILJS_USER_ID);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
